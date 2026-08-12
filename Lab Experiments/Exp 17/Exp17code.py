da3c406b54a4dcaff60b2cea86e3502669ea0bc4
Vaishnavee BV <vaishnaveebv777@gmail.com>
2026-08-12 13:28:58 +0530

    Create Exp17code.py

---
+++ Lab Experiments/Exp 17/Exp17code.py
@@ -0,0 +1,38 @@
+# SOBEL EDGE DETECTION - X AXIS
+
+import cv2
+import matplotlib.pyplot as plt
+from google.colab import files
+
+# Upload Image
+uploaded = files.upload()
+
+filename = list(uploaded.keys())[0]
+
+# Read Image
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Sobel X
+sobel_x = cv2.Sobel(gray,
+                    cv2.CV_64F,
+                    1,   # dx
+                    0,   # dy
+                    ksize=3)
+
+sobel_x = cv2.convertScaleAbs(sobel_x)
+
+# Display
+plt.figure(figsize=(10,5))
+
+plt.subplot(1,2,1)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("Original Image")
+plt.axis("off")
+
+plt.subplot(1,2,2)
+plt.imshow(sobel_x, cmap='gray')
+plt.title("Sobel X")
+plt.axis("off")
+
+plt.show()
